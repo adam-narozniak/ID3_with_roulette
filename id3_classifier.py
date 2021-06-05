2a081e5870905496f23edca4d5cbfc8a8ab821df
Adam <narozniak.adam4@gmail.com>
2021-06-05 20:49:01 +0200

    Fit and prediction added

--- id3_classifier.py
+++ id3_classifier.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import logging
 import entropy
+from tqdm import tqdm
 logger = logging.getLogger("ID3")
 
 class ID3Classifier:
@@ -35,24 +36,31 @@ class ID3Classifier:
         if self._fit == False:
             logger.exception("You can not use predict before fit.")
             sys.exit(1)
-        y = np.empty(X.shape[0])
+        y = []
 
         for idx, x in X.iterrows():
             current_node = self._root
             predicted = False
             while predicted is False:
+                prediction = current_node.get_prediction()
+                if prediction is not None:
+                    # leaf node reached; move to the next instance
+                    y.append(prediction)
+                    predicted = True
+                    break
                 split_feature_name = current_node.get_split_feature_name()
                 x_split_val = x[split_feature_name]
-                for child in current_node.get_childer():
+                for child in current_node.get_children():
                     prediction = child.get_prediction()
                     if prediction is not None:
                         # leaf node reached; move to the next instance
-                        y[idx] = prediction
+                        y.append(prediction)
                         predicted = True
                         break
                     if x_split_val == child.get_split_value():
                         current_node = child
                         break
+        return pd.Series(y)
 
 
 
@@ -72,7 +80,7 @@ class ID3Classifier:
         # instances are indistinguishable; return the only possible prediction
         if len(y.unique()) == 1:
             return Node(None, None, y.iloc[0])
-        elif len(X) == 0:
+        elif X.size == 0:
             return Node(None, None, self._most_frequent_y)  # TO CHECK: middle arg doesn't matter
 
         features = X.columns.values
@@ -87,7 +95,7 @@ class ID3Classifier:
             mask = X[best_feature] == feature_value
             sub_X = X[mask]
             sub_y = y[mask]
-            node = self._id3(sub_X, sub_y, feature_value)
+            node = self._id3(sub_X.drop(best_feature, axis=1), sub_y, feature_value)
             root_node.add_child(node)
         return root_node
 
--- main.py
+++ main.py
@@ -10,5 +10,15 @@ if __name__ == '__main__':
     X, y = io_utils.read_dataset("./data/spliceDTrainKIS.dat.txt")
     logger.info("Dataset loaded correctly")
     id3_classifier = ID3Classifier()
+    # X = pd.DataFrame([
+    #     ['A', 1],
+    #     ['B', 1],
+    #     ['B', 2],
+    #     ['B', 2],
+    #     ['B', 3]])
+    # y = pd.Series([0, 1, 1, 0, 1])
     id3_classifier.fit(X, y)
-    print(X, y)
+    logger.info("Classifier fit done")
+    y_pred = id3_classifier.predict(X)
+    print(f"accuracy: {np.sum(y_pred==y)/X.shape[0]}")
+
